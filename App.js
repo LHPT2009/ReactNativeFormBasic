4e8af4a4c643e46a5e53854c14e610d4426a6fc4
lehuynhphuongtung <lehuynhphuongtung02@gmail.com>
2022-08-30 19:21:27 +0700

    FormFE

--- App.js
+++ App.js
@@ -1,14 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from 'react-navigation';
+import explore from './screens/Explore'
+import inbox from './screens/Inbox'
+import saved from './screens/Saved'
+import trips from './screens/Trips'
+import profile from './screens/Profile'
+
+export default createBottomTabNavigator({
+  Explore: {
+    screen: explore,
+    navigationOptions: {
+      tabBarLabel: 'EXPLORE',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-search-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+  Saved: {
+    screen: saved,
+    navigationOptions: {
+      tabBarLabel: 'SAVED',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-heart-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+  Trips: {
+    screen: trips,
+    navigationOptions: {
+      tabBarLabel: 'TRIPS',
+      tabBarIcon: ({ tintColor }) => (
+        <Image source={require('./assets/icon.png')} style={{ height: 24, width: 24, tintColor: tintColor }} />
+      )
+    }
+  },
+  Inbox: {
+    screen: inbox,
+    navigationOptions: {
+      tabBarLabel: 'INBOX',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-chatbox-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+  Profile: {
+    screen: profile,
+    navigationOptions: {
+      tabBarLabel: 'PROFILE',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-person-outline" color={tintColor} size={24} />
+      )
+    }
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: 'red',
+    inactiveTintColor: 'grey',
+    style: {
+      backgroundColor: 'white',
+      borderTopWidth: 0,
+      shadowOffset: { width: 5, height: 3 },
+      shadowColor: 'black',
+      shadowOpacity: 0.5,
+      elevation: 5
+    }
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
